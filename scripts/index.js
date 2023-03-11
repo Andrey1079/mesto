93fd55ae2ae4c22f60703071080620d86493d129
Andrey Dunaev <dunayev@yandex.ru>
2023-03-11 11:09:28 +0700

    fix:chande js with notices

--- scripts/index.js
+++ scripts/index.js
@@ -7,8 +7,14 @@ let editProfileButton = document.querySelector(".profile__edit-profile-button");
 // нахожу кнопку закрытия попапа без сохранения
 let closePopupWindowButton = popupElement.querySelector(".popup__close-button");
 
-// нахожу поле ввода
+// нахожу форму
 let formInput = popupElement.querySelector(".popup__form");
+
+//нахожу поля ввода для имени и професии
+let formInputUserName = formInput.querySelector(".popup__form-item_type_name");
+let formInputUserProfession = formInput.querySelector(
+  ".popup__form-item_type_profession"
+);
 
 // нахожу элемент с текущим значением имени пользователя
 let userName = document.querySelector(".profile__user-name");
@@ -18,15 +24,8 @@ let userProfession = document.querySelector(".profile__user-profession");
 
 //ФУНКЦИЯ открытие окна попапа и задание значений для полей
 function addClassPopupVisible() {
-  formInput.querySelector(".popup__form-item_type_name").value = "";
-  formInput.querySelector(".popup__form-item_type_profession").value = "";
-
-  formInput
-    .querySelector(".popup__form-item_type_name")
-    .setAttribute("placeholder", userName.textContent);
-  formInput
-    .querySelector(".popup__form-item_type_profession")
-    .setAttribute("placeholder", userProfession.textContent);
+  formInputUserName.setAttribute("value", userName.textContent);
+  formInputUserProfession.setAttribute("value", userProfession.textContent);
   popupElement.classList.add("popup_visible");
 }
 
@@ -38,12 +37,8 @@ function closePopup() {
 // ФУНКЦИЯ отправки формы
 function handleFormSubmit(evt) {
   evt.preventDefault();
-  userName.textContent = formInput.querySelector(
-    ".popup__form-item_type_name"
-  ).value;
-  userProfession.textContent = formInput.querySelector(
-    ".popup__form-item_type_profession"
-  ).value;
+  userName.textContent = formInputUserName.value;
+  userProfession.textContent = formInputUserProfession.value;
   closePopup();
 }
 
