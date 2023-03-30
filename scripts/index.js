98cb66ccebe917362450e4dba1aa1b0b732c61e6
Andrey Dunaev <dunayev@yandex.ru>
2023-03-30 19:05:09 +0700

    feat: сделать валидацию форм с помощью JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,47 +1,43 @@
 //                                                            -----ОБЪЯВДЕНИЕ ПЕРЕМЕННЫХ-----
 const allClosePopupButtons = document.querySelectorAll(".popup__close-button");
 //---Редактирование профиля---
-const popupEditProfile = document.querySelector(".popup-edit-profile"); //попап редактирования профиля
+const popupEditProfile = document.querySelector(".popup-edit-profile");
+
 const editProfileButton = document.querySelector(
-  //кнопка редактирования профиля
   ".profile__edit-profile-button"
 );
-const formInputEditProfile = popupEditProfile.querySelector(".popup__form"); //форма редактирования профиля
+const formInputEditProfile = popupEditProfile.querySelector(".popup__form");
 const formInputUserName = formInputEditProfile.querySelector(
   ".popup__form-item_type_name"
-); //поле ввода имени пользователя
+);
 const formInputUserProfession = formInputEditProfile.querySelector(
   ".popup__form-item_type_profession"
-); //поле ввода профессии пользователя
-const userName = document.querySelector(".profile__user-name"); //значение имени пользователя
-const userProfession = document.querySelector(".profile__user-profession"); //значение профессии пользователя
-
-formInputEditProfile.addEventListener("keydown", (evt) => console.log(evt.key));
+);
+const userName = document.querySelector(".profile__user-name");
+const userProfession = document.querySelector(".profile__user-profession");
 
 //---Галерея---
-const popupAddPhoto = document.querySelector(".popup-add-photo"); //попап добавления карточки в галерею
+const popupAddPhoto = document.querySelector(".popup-add-photo");
 const galleryItemTemplate = document.querySelector(
   ".gallery-item-template"
-).content; //темплейт
-const galleryContainer = document.querySelector(".gallery__list"); //галерея
-const addPhotoButton = document.querySelector(".profile__add-photo-button"); //кнопка добавления фотографий
-const formInputAddPhoto = popupAddPhoto.querySelector(".popup__form"); //форма добавления новой карточки в галерею
+).content;
+const galleryContainer = document.querySelector(".gallery__list");
+const addPhotoButton = document.querySelector(".profile__add-photo-button");
+const formInputAddPhoto = popupAddPhoto.querySelector(".popup__form");
 const formInputPlace = formInputAddPhoto.querySelector(
   ".popup__form-item_type_place"
-); //поле ввода названия места
+);
 const formInputPlaceLink = formInputAddPhoto.querySelector(
   ".popup__form-item_type_img-link"
-); //поле ввода ссылки на фотографию
-
-//---Попап с увеличенной фотографией---
+);
 const popupBigPhoto = document.querySelector(".popup-big-photo"); //попап с увеличенной фотографией
 const popupBigPohotoImage = popupBigPhoto.querySelector(".popup__img"); //увеличенная фотография в поппапе
 const popupBigPhotoDescription = popupBigPhoto.querySelector(
   ".popup__place-name-of-big-photo"
-); //подпись к увеличенной фотографии в попапе
+);
 
 //                                                            -----ФУНКЦИИ-----
-//ФУНКЦИЯ создания карточки
+
 function renderCard(item) {
   const galleryElement = galleryItemTemplate
     .querySelector(".gallery__item")
@@ -53,14 +49,12 @@ function renderCard(item) {
   galeryCaption.textContent = item.name;
   return galleryElement;
 }
-
-// ФУНКЦИЯ добавления карточек в галерею
 function addNewGalleryCard(item) {
   galleryContainer.prepend(renderCard(item));
 }
-//ФУНКЦИЯ cоздания объекта галереи
+galleryArray.forEach(addNewGalleryCard);
+
 function addNewPlace(evt) {
-  evt.preventDefault();
   const newGalleryItem = {};
   newGalleryItem.name = formInputPlace.value;
   newGalleryItem.link = formInputPlaceLink.value;
@@ -68,52 +62,39 @@ function addNewPlace(evt) {
   closePopup(popupAddPhoto);
   formInputAddPhoto.reset();
 }
-// ФУНКЦИЯ удаления места из галлереи
 function deleteGalleryItem(item) {
   item.remove();
 }
-// ФУНКЦИЯ открытия большой фотографии
 function openBigGalleryItem(link, name) {
   openPopup(popupBigPhoto);
   popupBigPohotoImage.src = link;
   popupBigPohotoImage.alt = name;
   popupBigPhotoDescription.textContent = name;
 }
-
-//ФКНКЦИЯ медленного открытия
 function openPopup(item) {
   item.classList.add("popup_visible");
   item.addEventListener("click", closePopupByClickOverlay);
   document.addEventListener("keydown", checkPushEscape, item);
+  enableValidation(settings);
 }
-
-// ФУНКЦИЯ ПРОВЕРКИ НАЖАТИЯ ESCAPE
 function checkPushEscape(evt) {
   if (evt.key === "Escape") {
     closePopup(document.querySelector(".popup_visible"));
   }
 }
-
-// ФУНКЦИЯ медленного закрытия
 function closePopup(item) {
   item.classList.remove("popup_visible");
   item.addEventListener("click", closePopupByClickOverlay);
   document.removeEventListener("keydown", checkPushEscape);
 }
-
-// ФУНКЦИЯ отправки формы профиля юзера
 function editProfile(evt) {
-  evt.preventDefault();
   userName.textContent = formInputUserName.value;
   userProfession.textContent = formInputUserProfession.value;
   closePopup(popupEditProfile);
 }
-
-// ФУНКЦИЯ установки лайка
 function changeLikeButton(item) {
   item.classList.toggle("gallery__like-button_active");
 }
-// функция обработки клика по оверлэю
 function closePopupByClickOverlay(evt) {
   if (evt.target === evt.currentTarget) {
     closePopup(evt.currentTarget);
@@ -126,17 +107,17 @@ editProfileButton.addEventListener("click", function () {
   formInputUserName.value = userName.textContent;
   formInputUserProfession.value = userProfession.textContent;
   openPopup(popupEditProfile);
-}); // слушает нажатие кнопки редактирования профиля и заполняет значения формы ввода
+});
 
 addPhotoButton.addEventListener("click", function () {
   openPopup(popupAddPhoto);
-}); // слушает нажатие кнопки добавления фотографии и открывает попап добавления фотографии
-formInputEditProfile.addEventListener("submit", editProfile); //слушает нажатие кнопки добавить фото и добавляет фото в галерею
-formInputAddPhoto.addEventListener("submit", addNewPlace); //слушает нажатие кнопки добавить фото и добавляет фото в галерею
+});
+formInputEditProfile.addEventListener("submit", editProfile);
+formInputAddPhoto.addEventListener("submit", addNewPlace);
 allClosePopupButtons.forEach((button) => {
   const buttonsPopup = button.closest(".popup");
   button.addEventListener("click", () => closePopup(buttonsPopup));
-}); //перебирает все кнопки закрытия попапов и навешивает на них слушатели
+});
 galleryContainer.addEventListener("click", (evt) => {
   if (evt.target.classList.contains("gallery__like-button")) {
     changeLikeButton(evt.target);
@@ -152,4 +133,3 @@ galleryContainer.addEventListener("click", (evt) => {
   }
 });
 //                                                            -----ФОРМИРОВАНИЕ ГАЛЕРЕИ ИЗ МАССИВА-----
-galleryArray.forEach(addNewGalleryCard);
--- scripts/validate.js
+++ scripts/validate.js
@@ -2,7 +2,70 @@ const settings = {
   formSelector: ".popup__form",
   inputSelector: ".popup__form-item",
   submitButtonSelector: ".popup__submit-button",
-  inactiveButtonClass: ".popup__submit-button_disabled",
-  inputErrorClass: ".popup__form-item_type_error",
-  errorClass: ".popup__error_visible",
+  inactiveButtonClass: "popup__submit-button_disabled",
+  errorClass: "popup__form-item-error_visible",
 };
+
+function enableValidation({ formSelector, ...set }) {
+  Array.from(document.querySelectorAll(formSelector)).forEach((form) => {
+    form.addEventListener("submit", (evt) => evt.preventDefault());
+    setEventListener(form, set);
+  });
+}
+
+function setEventListener(
+  form,
+  { inputSelector, submitButtonSelector, inactiveButtonClass, ...set }
+) {
+  const inputs = Array.from(form.querySelectorAll(inputSelector));
+  toggleButtonState(form, inputs, submitButtonSelector, inactiveButtonClass);
+  inputs.forEach((input) => {
+    checkInputValidity(form, input, set);
+    addEventListener("input", () => {
+      checkInputValidity(form, input, set);
+      toggleButtonState(
+        form,
+        inputs,
+        submitButtonSelector,
+        inactiveButtonClass
+      );
+    });
+  });
+}
+
+function checkInputValidity(form, input, { errorClass }) {
+  const messageError = form.querySelector(`.${input.id}-error`);
+  console.log("hi");
+  if (input.validity.valid) {
+    hideInputError(messageError, errorClass);
+  } else {
+    showInputError(messageError, input.validationMessage, errorClass);
+  }
+}
+function showInputError(messageError, message, errorClass) {
+  messageError.textContent = message;
+  messageError.classList.add(errorClass);
+}
+function hideInputError(messageError, errorClass) {
+  messageError.classList.remove(errorClass);
+}
+
+function toggleButtonState(
+  form,
+  inputs,
+  submitButtonSelector,
+  inactiveButtonClass
+) {
+  const submitButton = form.querySelector(submitButtonSelector);
+  if (hasInvalidInput(inputs)) {
+    submitButton.classList.add(inactiveButtonClass);
+    submitButton.setAttribute("disabled", "disabled");
+  } else {
+    submitButton.classList.remove(inactiveButtonClass);
+    submitButton.removeAttribute("disabled", "disabled");
+  }
+}
+
+function hasInvalidInput(inputs) {
+  return inputs.some((input) => !input.validity.valid);
+}
